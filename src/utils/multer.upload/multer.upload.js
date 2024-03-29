e406911e2be06efe38c106957a747ba40e29534d
Matias Bidart <matiasezequielbidart@gmail.com>
2024-03-29 13:55:41 -0300

    BE 02.3 Multer, edit path on render

--- src/utils/multer.upload/multer.upload.js
+++ src/utils/multer.upload/multer.upload.js
@@ -7,9 +7,8 @@ const storage = multer.diskStorage({
     },
     filename: function(req, file, cb) {
       cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-      console.log('🎈🎈🎈')
-      console.log(file)
     }
+    
   });
   
   const upload = multer({ storage: storage });
@@ -17,8 +16,6 @@ const storage = multer.diskStorage({
   exports.uploadFile = upload.single('file');
   
   exports.getFileUrl = (req, res) => {
-    const fileUrl = process.env.NODE_ENV === 'production' ? 'https://notables-backend.onrender.com/api/v1/' + req.file.patch : 'http://localhost:9000/' + req.file.path;
+    const fileUrl = process.env.NODE_ENV === 'production' ? 'https://notables-backend.onrender.com/api/v1/' + req.file.path : 'http://localhost:9000/' + req.file.path;
     res.json({ url: fileUrl });
-    console.log('🎄🎄🎄')
-    console.log(fileUrl)
   };
