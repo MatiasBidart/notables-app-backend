1d246dd58244449a99a6201b4250e5b46c65acab
Matias Bidart <matiasezequielbidart@gmail.com>
2024-03-29 14:00:53 -0300

    BE 03.4 Multer, edit path and declaring GET route

--- src/utils/multer.upload/upload.route.js
+++ src/utils/multer.upload/upload.route.js
@@ -2,6 +2,7 @@ const upload= require('./multer.upload');
 const router = require('express').Router();
 
 router.route('/')
+    .get((req,res)=> {res.status(200).json({message: 'Here we upload files! please post to:', link: 'https://notables-backend.onrender.com/api/v1/upload'})})
     .post(upload.uploadFile, upload.getFileUrl);
 
 module.exports = router;
